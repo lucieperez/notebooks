b25fec14753f76ec0456ea57497042fe72ccea4f
Lucie <perez.lucie970@gmail.com>
2023-12-08 10:11:53 +0100

    add pytest file and notebook

---
+++ tests/test_datasets.py
@@ -0,0 +1,27 @@
+# Need a folder test at the root ot the project
+# create a .py file, name starts with test
+# each function title starts with test and explain what you test
+
+
+import os
+import pytest
+import pandas as pd
+
+
+@pytest.fixture(scope="module")
+def input_df():
+    df = pd.read_csv("C:/Users/perez/Documents/github/notebooks/1_annotation_tools/data/annotation_df_history/isaiah_dataset.csv")
+    return df
+
+
+def test_lines(input_df):
+    assert input_df.shape[0] > 0
+
+
+def test_motion_type_allowed_values(input_df):
+    allowed_values = {"factive", "fictive", "factive fictive", "other", "unknown"}
+    assert all([value in allowed_values for value in set(input_df.motion_type) if isinstance(value, str)])
+
+def test_name(input_df):
+    assert all([len(text_str) == len(sign_str) for text_str, sign_str in zip(input_df.gcons_verse, input_df.sign_info) if isinstance(sign_str, str)])
+    
